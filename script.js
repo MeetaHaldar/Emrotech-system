7eee9e3bbcfc01594be81c8aa4d825a30ad93355
meeta haldar <meetahaldar1001@gmail.com>
2022-06-18 21:26:22 +0530

    password validation completed

--- script.js
+++ script.js
@@ -2,28 +2,38 @@ var pass1 = document.getElementById("pass1");
 var pass2 = document.getElementById("pass2");
 var validate = document.getElementById("p");
 var btn = document.getElementById("btn");
-
+var form = document.getElementById("form");
+btn.disabled = true;
+if (pass1.value || pass2.value == " ") {
+  btn.disabled = true;
+}
 pass1.onkeyup = function () {
-  // Validate lowercase letters
   var validations = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g;
 
   if (pass1.value.length >= 8) {
     if (pass1.value.match(validations)) {
       validate.innerHTML = "strong password";
     } else {
-      var alphabet = /\s?[^a-z0-9\_][^a-z0-9\_]/i / g;
-      if (myInput.value.match(alphabet)) {
-        validate.innerHTML = "add atleast one number";
-      } else {
-        if (pass1.value == pass2.value) {
-          validate.innerHTML = "password matches";
-        } else {
-          validate.innerHTML = "password is not same";
-        }
+      var alphabet = /^[A-Z]+$/i;
+      var numbers = /^[0-9]/;
+      if (pass1.value.match(alphabet)) {
+        validate.innerHTML =
+          "the password contains characters only from the alphabet add atleast one number";
+      } else if (pass1.value.match(numbers)) {
+        validate.innerHTML = "add atleast one character";
+      }
+      else {
+        btn.disabled = false;
       }
     }
   } else {
-    validate.innerHTML = "this must be of length of 8 ";
+    validate.innerHTML = "password length is less than 8 ";
   }
-
 };
+btn.addEventListener("click", () => {
+  if (pass1.value == pass2.value) {
+    validate.innerHTML = "password matches";
+  } else {
+    validate.innerHTML = "the 2 passwords do not match";
+  }
+});
