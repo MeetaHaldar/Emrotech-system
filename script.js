e042a922f0645dd8056f6de7d125da52597bc753
meeta haldar <meetahaldar1001@gmail.com>
2022-06-18 21:43:19 +0530

    completed

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var validate = document.getElementById("p");
 var btn = document.getElementById("btn");
 var form = document.getElementById("form");
 btn.disabled = true;
+form.style.display = "block";
 if (pass1.value || pass2.value == " ") {
   btn.disabled = true;
 }
@@ -13,6 +14,8 @@ pass1.onkeyup = function () {
   if (pass1.value.length >= 8) {
     if (pass1.value.match(validations)) {
       validate.innerHTML = "strong password";
+      pass1.style.borderColor = "green";
+      btn.disabled = false;
     } else {
       var alphabet = /^[A-Z]+$/i;
       var numbers = /^[0-9]/;
@@ -21,18 +24,20 @@ pass1.onkeyup = function () {
           "the password contains characters only from the alphabet add atleast one number";
       } else if (pass1.value.match(numbers)) {
         validate.innerHTML = "add atleast one character";
-      }
-      else {
+      } else {
         btn.disabled = false;
       }
     }
   } else {
+    pass1.style.borderColor = "red";
     validate.innerHTML = "password length is less than 8 ";
   }
 };
 btn.addEventListener("click", () => {
   if (pass1.value == pass2.value) {
     validate.innerHTML = "password matches";
+    form.style.display = "none";
+    final.style.display = "block";
   } else {
     validate.innerHTML = "the 2 passwords do not match";
   }
